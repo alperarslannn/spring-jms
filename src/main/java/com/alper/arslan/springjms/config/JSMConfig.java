f6aab34624276a0acc2a926daf583388b146a707
Arslan, Alper <alper.arslan@adesso.com.tr>
2023-09-17 12:13:33 +0300

    added Message Converter config

---
+++ src/main/java/com/alper/arslan/springjms/config/JSMConfig.java
@@ -0,0 +1,19 @@
+package com.alper.arslan.springjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JSMConfig {
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
