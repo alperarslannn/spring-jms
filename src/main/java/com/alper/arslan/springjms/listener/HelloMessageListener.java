f7dccfe91079c423d9db1c7360838d587bc8e46d
Arslan, Alper <alper.arslan@adesso.com.tr>
2023-09-17 15:22:37 +0300

    added sendAndReceive queue

--- src/main/java/com/alper/arslan/springjms/listener/HelloMessageListener.java
+++ src/main/java/com/alper/arslan/springjms/listener/HelloMessageListener.java
@@ -2,21 +2,37 @@ package com.alper.arslan.springjms.listener;
 
 import com.alper.arslan.springjms.config.JmsConfig;
 import com.alper.arslan.springjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class HelloMessageListener {
+    private final JmsTemplate jmsTemplate;
+
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listener(@Payload HelloWorldMessage helloWorldMessage,
                          @Headers MessageHeaders headers, Message message) {
-        System.out.println("I got a message!!!!");
-        System.out.println(helloWorldMessage);
+        /*System.out.println("I got a message!!!!");
+        System.out.println(helloWorldMessage);*/
+
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenerForHello(@Payload HelloWorldMessage helloWorldMessage,
+                         @Headers MessageHeaders headers, Message message) throws JMSException {
+        HelloWorldMessage payloadMsg = HelloWorldMessage.builder().id(UUID.randomUUID()).message("World!").build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
 
     }
 }
--- src/main/java/com/alper/arslan/springjms/sender/HelloSender.java
+++ src/main/java/com/alper/arslan/springjms/sender/HelloSender.java
@@ -2,24 +2,56 @@ package com.alper.arslan.springjms.sender;
 
 import com.alper.arslan.springjms.config.JmsConfig;
 import com.alper.arslan.springjms.model.HelloWorldMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class HelloSender {
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper mapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage(){
-        System.out.println("I'm sending message");
+        //System.out.println("I'm sending message");
         HelloWorldMessage message = HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello world!").build();
 
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
-        System.out.println("Message sent.");
+        //System.out.println("Message sent.");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceiveMessage() throws JMSException {
+        System.out.println("I'm sending message");
+
+        HelloWorldMessage helloWorldMessage = HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello").build();
+
+        Message receivedMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(mapper.writeValueAsString(helloWorldMessage));
+                    helloMessage.setStringProperty("_type","com.alper.arslan.springjms.model.HelloWorldMessage");
+                    System.out.println("Sending hello");
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException("boom");
+                }
+
+            }
+        });
+
+        System.out.println(receivedMessage.getBody(String.class));
     }
 }
